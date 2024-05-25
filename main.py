ce8e3669ddc9513a5d93572fc8da36eb3142f3ee
acymz <cymz6@hotmail.com>
2024-05-25 13:19:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ if __name__ == "__main__":
     fetch_and_save('https://quan.suning.com/getSysTime.do', 'data/Script_execution_time.txt')
     # 获取TVBox肥猫接口
     fetch_and_save('http://xn--z7x900a.live/', 'data/肥猫.txt', 'okhttp/3.12.11')
-    # 获取TVBox欧歌接口
-    fetch_and_save('http://tv.nxog.top/m/111.php', 'data/欧歌.txt', 'okhttp/3.12.11')
     # 获取TVBox巧技接口
     fetch_and_save('http://cdn.qiaoji8.com/tvbox.json', 'data/巧技.txt', 'okhttp/3.12.11')
     # 获取TVBox饭太硬接口
